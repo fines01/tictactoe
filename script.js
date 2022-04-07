f2974f74c4802582916ae3e1f6d1b6bcae03fd7a
I. Fritsch <ines.fritsch@gmail.com>
2022-04-07 22:10:50 +0200

    Format and cleanup

--- script.js
+++ script.js
@@ -3,34 +3,32 @@ let gameOver = false;
 let winner;
 let currentShape = 'cross';
 let scope = 3; // Level könnten später vl angepasst werden
-
 let singlePlayer = false;
 
-let winPatterns = [
-    [0,1,2],[3,4,5],[6,7,8],
-    [0,3,6],[1,4,7],[2,5,8],
-    [0,4,8],[2,4,6]
+let winPatterns = 
+[ 
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
 ]; // for level 1, scope = 3
 
 // TODO(idea) getWinPatterns() (determine winning pattern, for possible variable field numbers/levels later?)
 
-function renderGameOverScreen(){
+function renderGameOverScreen() {
     let screen = document.getElementById('game-over');
     gameOverScreenHTML(screen);
 }
 
 // get actual array length without empty values
-function getArrLength(arr){
-    let count=0;
-    for(let i=0; i< arr.length; i++){
-        if(arr[i] !== undefined){
+function getArrLength(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== undefined) {
             count++;
         }
     }
     return count;
 }
 
-function getEmptyField(){
+function getEmptyField() {
     let emptyFieldsIndexes = [];
     let randomIndex;
 
@@ -46,14 +44,14 @@ function getEmptyField(){
 }
 
 function hideElement(...elementIDs) {
-    for(let i = 0; i< elementIDs.length; i++){
-        if (document.getElementById(elementIDs[i])){
+    for (let i = 0; i < elementIDs.length; i++) {
+        if (document.getElementById(elementIDs[i])) {
             document.getElementById(elementIDs[i]).classList.add('d-none');
         }
     }
 }
 
-function showElement(...elementIDs){
+function showElement(...elementIDs) {
     for (let i = 0; i < elementIDs.length; i++) {
         document.getElementById(elementIDs[i]).classList.remove('d-none');
     }
@@ -66,7 +64,7 @@ function hideLines() {
 }
 
 // generate table
-function makeTable(table){
+function makeTable(table) {
     // generate rows
     for (let i = 0; i < scope; i++) {
         let row = table.insertRow(i);
@@ -79,7 +77,7 @@ function makeTable(table){
 }
 
 // fill table fields
-function populateTableContent(){
+function populateTableContent() {
     let gameFields = document.getElementsByClassName('game-field');
     for (let i = 0; i < gameFields.length; i++) {
         gameFields[i].id = i;
@@ -91,22 +89,28 @@ function populateTableContent(){
     }
 }
 
-function renderGamefield(){
+function renderGamefield() {
     let table = document.getElementById('gamefield');
     table.innerHTML = '';
     makeTable(table);
     populateTableContent();
 }
 
-function changePlayer() {
+function changePlayer() { // todo: refactor
+    let player1 = document.getElementById('player-1');
+    let player2 = document.getElementById('player-2');
+
+    // renderPlayerPanel();
+    playerPanelHTML(player1, player2);
+
     if (currentShape == 'cross') {
-        document.getElementById('player-2').classList.remove('player-inactive');
-        document.getElementById('player-1').classList.add('player-inactive');
         currentShape = 'circle';
+        player2.classList.remove('player-inactive');
+        player1.classList.add('player-inactive');
     } else {
-        document.getElementById('player-1').classList.remove('player-inactive');
-        document.getElementById('player-2').classList.add('player-inactive');
         currentShape = 'cross';
+        player1.classList.remove('player-inactive');
+        player2.classList.add('player-inactive');
     }
 }
 
@@ -119,27 +123,27 @@ function executeMove() {
         drawShape();
         checkForWin();
         // 2. case singlePlayer: generate an automatic move
-        if(singlePlayer && !gameOver) {
+        if (singlePlayer && !gameOver) {
             generateRandomMove();
             // TODO (idea): generate calculated move, and do a random OR calculated move (so it won't be impossible or too hard to win)
-            // Math.random() < 0.5 ? generateRandomMove() : generateCalculatedMove() // Ternäre Operatoren sehr angenehm (adjust 0.5 je nach Schwierigkeitsgrad,zB)
+            // Math.random() < 0.5 ? generateRandomMove() : generateCalculatedMove() // Ternäre Operatoren, sehr angenehm (adjust 0.5 je nach Schwierigkeitsgrad,zB)
         }
     }
 }
 
 // generate a completely random move
-function generateRandomMove(){
+function generateRandomMove() {
     changePlayer();
     fields[getEmptyField()] = currentShape;
-    setTimeout( function(){
+    setTimeout(function () {
         drawShape();
     }, 500);
     checkForWin();
 }
 
 function drawShape() {
-    for( let i=0; i<fields.length; i++){
-        if(fields[i] == 'circle'){
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circle') {
             showElement(`circle-${i}`);
         }
         if (fields[i] == 'cross') {
@@ -164,7 +168,7 @@ function checkWinner() {
     for (let i = 0; i < winPatterns.length; i++) {
         let pattern = winPatterns[i];
         // check respective pattern & evaluation if field is filled (no undefined)
-        if (fields[pattern[0]] == fields[pattern[1]] && fields[pattern[1]] == fields[pattern[2]] && fields[pattern[0]]) { 
+        if (fields[pattern[0]] == fields[pattern[1]] && fields[pattern[1]] == fields[pattern[2]] && fields[pattern[0]]) {
             winner = fields[pattern[0]];
             drawLine(i);
             return true;
@@ -173,18 +177,19 @@ function checkWinner() {
 }
 
 function checkUndecided() {
-     if (getArrLength(fields) === ((scope * scope)) && !winner) {
-         document.getElementById('player-1').classList.add('player-inactive');
-         document.getElementById('player-2').classList.add('player-inactive');
-         return true;
-     }
+    if (getArrLength(fields) === ((scope * scope)) && !winner) {
+        return true;
+    }
 }
 
-function checkForWin(){ 
+function checkForWin() {
     // do game-over stuff (screen etc)
-    if (checkWinner() || checkUndecided() ){
+    if (checkWinner() || checkUndecided()) {
+        // renderPlayerPanel(true);
+        document.getElementById('player-1').classList.add('player-inactive'); // in FKT renderPlayerPanel() ?
+        document.getElementById('player-2').classList.add('player-inactive');
         gameOver = true;
-        setTimeout( function () {
+        setTimeout(function () {
             showElement('game-over');
             renderGameOverScreen();
         }, 1000);
@@ -200,19 +205,21 @@ function restartGame() {
     // reset game-field (hide game-over screen, line and shapes)
     hideElement('game-over', 'restart-button');
     hideLines();
-    for ( let i = 0; i < (scope*scope); i++ ) {
+    for (let i = 0; i < (scope * scope); i++) {
         hideElement(`circle-${i}`, `cross-${i}`);
     }
+    if(!winner){ changePlayer(); }
 }
 
 function setPlayerMode(bool) {
     restartGame();
+    changePlayer();
     singlePlayer = bool;
 }
 
 function setActiveLink(x) {
     let links = document.getElementsByClassName('nav-link');
-    for (let i=0; i< links.length; i++){
+    for (let i = 0; i < links.length; i++) {
         links[i].classList.remove('active-link');
     }
     links[x].classList.add('active-link');
